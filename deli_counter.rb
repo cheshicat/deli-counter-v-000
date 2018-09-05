5cb92dcf4c4f1b2dfa9fa3a9a6846ba54c2a13a3
Allison Howard <alliemhow@gmail.com>
2018-09-05 10:15:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 def line(katz_deli)
  if katz_deli = []
    puts "The line is currently empty."
+ else
+   katz_deli.each_with_index do |customer_name, index|
+     puts "#{index + 1}. #{customer_name}"
 
+   end
  end
 end
